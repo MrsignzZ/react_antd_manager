b92430ce9e282d7deaeb55d56bc7c28bce51fced
1440806960@qq.come <1440806960@qq.com>
2019-01-28 21:42:13 +0800

    订单基础信息实现

--- src/router.js
+++ src/router.js
@@ -19,6 +19,7 @@ import HighTable from './pages/table/highTable';
 import City from './pages/city';
 import Order from './pages/order';
 import Common from './common';
+import OrderDetail from './pages/order/detail'
 
 export default class Router extends React.Component {
   render() {
@@ -52,7 +53,7 @@ export default class Router extends React.Component {
           />
           <Route path="/common" render={() => (
             <Common>
-              <Route path="/common/order/detail/:orderId" component={Login}/>
+              <Route path="/common/order/detail/:orderId" component={OrderDetail}/>
             </Common>
           )}/>
         </App>
