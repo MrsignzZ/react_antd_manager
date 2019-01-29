9a0ea5ce2f55724ad788df74ce6488635de25bdf
1440806960@qq.come <1440806960@qq.com>
2019-01-29 21:58:09 +0800

    调用百度地图API完成地图显示路线和区域

--- src/pages/order/detail.js
+++ src/pages/order/detail.js
@@ -9,7 +9,7 @@ export default class OrderDetail extends React.Component {
   componentDidMount() {
     let orderId = this.props.match.params.orderId;
     if (orderId) {
-      this.getDetailInfo(orderId)
+      this.getDetailInfo(orderId);
     }
   }
 
@@ -28,8 +28,101 @@ export default class OrderDetail extends React.Component {
           this.setState({
             orderInfo: res.result
           });
+          this.renderMap(res.result);
         }
       });
+  };
+
+  renderMap = result => {
+    this.map = new window.BMap.Map('orderDetailMap');
+    // 添加地图控件
+    this.addMapControl();
+    // 调用路线图绘制方法
+    this.drawBikeRoute(result.position_list);
+    this.drawServiceArea(result.area);
+  };
+
+  // 添加地图控件
+  addMapControl = () => {
+    let map = this.map;
+    map.addControl(
+      new window.BMap.ScaleControl({ anchor: window.BMAP_ANCHOR_TOP_RIGHT })
+    );
+    map.addControl(
+      new window.BMap.NavigationControl({
+        anchor: window.BMAP_ANCHOR_TOP_RIGHT
+      })
+    );
+  };
+
+  // 绘制行驶路线图
+  drawBikeRoute = positionList => {
+    let map = this.map;
+    let startPoint = '';
+    let endPoint = '';
+    if (positionList.length > 0) {
+      let first = positionList[0];
+      let last = positionList[positionList.length - 1];
+      startPoint = new window.BMap.Point(first.lon, first.lat);
+      let startIcon = new window.BMap.Icon(
+        '/assets/start_point.png',
+        new window.BMap.Size(36, 42),
+        {
+          imageSize: new window.BMap.Size(36, 42),
+          anchor: new window.BMap.Size(36, 42)
+        }
+      );
+      let startMarker = new window.BMap.Marker(startPoint, { icon: startIcon });
+      // 添加起点图标到地图上
+      map.addOverlay(startMarker);
+
+      endPoint = new window.BMap.Point(last.lon, last.lat);
+      let endIcon = new window.BMap.Icon(
+        '/assets/end_point.png',
+        new window.BMap.Size(36, 42),
+        {
+          imageSize: new window.BMap.Size(36, 42),
+          anchor: new window.BMap.Size(36, 42)
+        }
+      );
+      let endMarker = new window.BMap.Marker(endPoint, { icon: endIcon });
+      // 添加终点图标到地图上
+      map.addOverlay(endMarker);
+
+      // 连接路线图
+      let trackPoint = [];
+      for (let i = 0; i < positionList.length; i++) {
+        let point = positionList[i];
+        trackPoint.push(new window.BMap.Point(point.lon, point.lat));
+      }
+
+      let polyline = new window.BMap.Polyline(trackPoint, {
+        strokeColor: '#1869ad',
+        strokeWeight: 3,
+        strokeOpacity: 1
+      });
+      this.map.addOverlay(polyline);
+
+      // 把终点设为地图中心
+      this.map.centerAndZoom(endPoint, 11);
+    }
+  };
+  
+  // 绘制服务区
+  drawServiceArea = positionList => {
+    let trackPoint = [];
+    for (let i = 0; i < positionList.length; i++) {
+      let point = positionList[i];
+      trackPoint.push(new window.BMap.Point(point.lon, point.lat));
+    }
+    let polygon = new window.BMap.Polygon(trackPoint, {
+      strokeColor: '#ce0000',
+      strokeWeight: 4,
+      strokeOpacity: 1,
+      fillColor: '#ff8605',
+      fillOpacity: 0.4
+    });
+    this.map.addOverlay(polygon);
   };
 
   render() {
@@ -38,7 +131,7 @@ export default class OrderDetail extends React.Component {
     return (
       <div>
         <Card>
-          <div id="orderDetailMap" />
+          <div id="orderDetailMap" className="order-map" />
           <div className="detail-items">
             <div className="item-title">基础信息</div>
             <ul className="detail-form">
