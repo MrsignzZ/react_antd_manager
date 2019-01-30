ab79021674b7432db814d2d3e00ca345266bcc03
1440806960@qq.come <1440806960@qq.com>
2019-01-30 20:08:02 +0800

    axios请求封装

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,32 @@
 import JsonP from 'jsonp';
 import axios from 'axios';
 import { Modal } from 'antd';
+import Utils from '../utils/utils'
 export default class Axios {
+  static requestList(_this, url, params, isMock) {
+    var data = {
+      params: params,
+      isMock
+    }
+    this.ajax({
+      url,
+      data
+    }).then((data) => {
+      if (data && data.result) {
+        let list = data.result.item_list.map((item, index) => {
+          item.key = index;
+          return item;
+        });
+        _this.setState({
+          list: list,
+          pagination: Utils.pagination(data, current => {
+            _this.params.page = current;
+            _this.requestList();
+          })
+        });
+      }
+    })
+  }
   static jsonp(options) {
     return new Promise((resolve, reject) => {
       JsonP(
@@ -27,8 +52,15 @@ export default class Axios {
       loading.style.display = 'block';
     }
     // let baseApi = 'https://www.easy-mock.com/mock/5a7278e28d0c633b9c4adbd7/api';
-    let baseApi =
+    let baseApi = ''
+    if (options.isMock) {
+      baseApi =
       'https://www.easy-mock.com/mock/5c4d60b9857c7f670bc96b63/mockapi';
+    } else {
+      baseApi =
+        'https://www.easy-mock.com/mock/5c4d60b9857c7f670bc96b63/mockapi';
+    }
+
     return new Promise((resolve, reject) => {
       axios({
         url: options.url,
